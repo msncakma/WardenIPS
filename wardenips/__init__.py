35bd13bfe924251e7fcc5e621296a4dd01edcdca
msncakma <msn.gmgm@gmail.com>
2026-03-14 13:55:49 +0300

    chore: bump version to 0.3.0-beta-9

--- wardenips/__init__.py
+++ wardenips/__init__.py
@@ -6,5 +6,5 @@ Linux tabanlı sunucular için botnet ve DDoS saldırılarını
 tespit edip otonom savunma yapan yüksek performanslı IPS yazılımı.
 """
 
-__version__ = "0.3.0-beta-8"
+__version__ = "0.3.0-beta-9"
 __author__ = "msncakma <3"
